a2b3adb1ce7440dadc4a34cf959164d73defba2c
Zarko Selak <zarko.selak@trikoder.net>
2016-06-18 17:17:37 +0200

    Cleanup & webpack update - adds webpack config dev

--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Router, Route, Link, browserHistory } from 'react-router'
-
-import App from './app.js'
-import Login from './components/login/login.js'
-
-
-render((
-  <Router history={browserHistory}>
-    <Route path="/" component={Login}></Route>
-    <Route path="/app" component={App}></Route>
-  </Router>
-), document.getElementById('app'));
+console.log('trolololo!');
